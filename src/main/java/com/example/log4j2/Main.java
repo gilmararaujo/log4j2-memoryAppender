8dd0e202dac8ec7e1a039632faaef7a67d162c23
garaujo <liukangil@gmail.com>
2022-05-09 10:30:10 -0300

    Adjust blank space and comments.

--- src/main/java/com/example/log4j2/Main.java
+++ src/main/java/com/example/log4j2/Main.java
@@ -1,24 +1,19 @@
 package com.example.log4j2;
 
 import java.io.File;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
-
 public class Main {
     static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
         reconfigureLog4j2Path("config/log4j2.xml");
         System.setProperty("-DLog4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
-
-        //Uma aplicaï¿½ï¿½o simples que mostra o log no console
+        //Show logs in console
         LOGGER.debug("This is debug");
         LOGGER.info("This is info");
         LOGGER.error("This is error");
     }
-
     private static void reconfigureLog4j2Path(String args) {
         //reconfigure log4j2 in code with a specific configuration file
         LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
--- src/main/java/com/example/log4j2/MemoryAppender.java
+++ src/main/java/com/example/log4j2/MemoryAppender.java
@@ -9,21 +9,17 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
-
 @Plugin(name="MemoryAppender", category=Core.CATEGORY_NAME, elementType=Appender.ELEMENT_TYPE)
 public class MemoryAppender extends AbstractAppender {
     protected MemoryAppender(String name, Filter filter) {
         super(name, filter, null, false, null);
     }
-
     @PluginFactory
     public static MemoryAppender createAppender(@PluginAttribute("name") String name, @PluginElement("Filter") Filter filter) {
         return new MemoryAppender(name, filter);
     }
-
     public void append(LogEvent event) {
         // TODO Auto-generated method stub
-        System.out.println("ENTROU APPEND");
+        System.out.println("logback loading test appenders");
     }
-
 }
